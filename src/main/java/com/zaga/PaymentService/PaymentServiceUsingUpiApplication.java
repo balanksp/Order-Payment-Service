066b9193af1d9678e0fb0ccaa91f82f50f32111d
balanksp <a.bala101@gmail.com>
2023-07-26 14:03:50 +0530

    create a structure

---
+++ src/main/java/com/zaga/PaymentService/PaymentServiceUsingUpiApplication.java
@@ -0,0 +1,13 @@
+package com.zaga.PaymentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceUsingUpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceUsingUpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaga/PaymentService/PaymentServiceUsingUpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaga.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceUsingUpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
